f8f5de0d495430828d420d313c84595d207fa928
Vinson Chuong <vinsonchuong@gmail.com>
2026-08-23 12:41:04 -0700

    fix(xo_d): Move timing-dependent logic into server

--- server.mjs
+++ server.mjs
@@ -1,5 +1,12 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
 import {Xo} from 'xo'
 import formatterPretty from 'eslint-formatter-pretty'
+import {pathExists} from 'path-exists'
+import findCacheDirectory from 'find-cache-directory'
+
+const cacheDirName = 'xo-linter'
+const tsExtensions = new Set(['ts', 'tsx', 'cts', 'mts'])
 
 export default async function run([
   cliOptions,
@@ -47,21 +54,64 @@ export default async function run([
   }
 
   if (cliOptions.stdin) {
-    if (linterOptions.fix) {
-      const {
-        results: [result],
-      } = await xo.lintText(stdin, {
-        filePath: cliOptions.stdinFilename,
-      })
-      return result?.output ?? stdin
+    let shouldRemoveStdInFile = false
+    // For TypeScript, we need a file on the filesystem to lint it or else @typescript-eslint will blow up.
+    // We create a temporary file in the node_modules/.cache/xo-linter directory to avoid conflicts with the user's files and lint that file as if it were the stdin input as a work around.
+    // We clean up the file after linting.
+    if (
+      cliOptions.stdinFilename !== '' &&
+      tsExtensions.has(path.extname(cliOptions.stdinFilename).slice(1))
+    ) {
+      const absoluteFilePath = path.resolve(
+        cliOptions.cwd,
+        cliOptions.stdinFilename,
+      )
+      if (!(await pathExists(absoluteFilePath))) {
+        const cacheDir =
+          findCacheDirectory({name: cacheDirName, cwd: linterOptions.cwd}) ??
+          path.join(cliOptions.cwd, 'node_modules', '.cache', cacheDirName)
+        cliOptions.stdinFilename = path.join(
+          cacheDir,
+          path.basename(absoluteFilePath),
+        )
+        shouldRemoveStdInFile = true
+        baseXoConfigOptions.ignores = [
+          '!**/node_modules/**',
+          '!node_modules/**',
+          '!node_modules/',
+          `!${path.relative(cliOptions.cwd, cliOptions.stdinFilename)}`,
+        ]
+        if (!(await pathExists(path.dirname(cliOptions.stdinFilename)))) {
+          await fs.mkdir(path.dirname(cliOptions.stdinFilename), {
+            recursive: true,
+          })
+        }
+
+        await fs.writeFile(cliOptions.stdinFilename, stdin)
+      }
     }
 
-    return log(
-      await xo.lintText(stdin, {
-        filePath: cliOptions.stdinFilename,
-        warnIgnored: false,
-      }),
-    )
+    try {
+      if (linterOptions.fix) {
+        const {
+          results: [result],
+        } = await xo.lintText(stdin, {
+          filePath: cliOptions.stdinFilename,
+        })
+        return result?.output ?? stdin
+      }
+
+      return log(
+        await xo.lintText(stdin, {
+          filePath: cliOptions.stdinFilename,
+          warnIgnored: false,
+        }),
+      )
+    } finally {
+      if (shouldRemoveStdInFile) {
+        await fs.rm(cliOptions.stdinFilename)
+      }
+    }
   }
 
   const report = await xo.lintFiles(input)
